da586118b8447c6357d4e7b7ca956b50079eb03e
Daniel Springwald <daniel@springwald.de>
2018-01-12 23:33:53 +0100

    added roobert master control program, face gfx, speech output

---
+++ Python/FaceGfx.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+
+#      Roobert - home robot project
+#      ________            ______             _____ 
+#      ___  __ \______________  /_______________  /_
+#      __  /_/ /  __ \  __ \_  __ \  _ \_  ___/  __/
+#      _  _, _// /_/ / /_/ /  /_/ /  __/  /   / /_  
+#      /_/ |_| \____/\____//_.___/\___//_/    \__/
+#
+#     Project website: http://roobert.springwald.de
+#
+#     ################################
+#     # Roobert face graphics module #
+#     ################################
+#
+#     Licensed under MIT License (MIT)
+#
+#     Copyright (c) 2017 Daniel Springwald | daniel@springwald.de
+#
+#     Permission is hereby granted, free of charge, to any person obtaining
+#     a copy of this software and associated documentation files (the
+#     "Software"), to deal in the Software without restriction, including
+#     without limitation the rights to use, copy, modify, merge, publish,
+#     distribute, sublicense, and/or sell copies of the Software, and to permit
+#     persons to whom the Software is furnished to do so, subject to
+#     the following conditions:
+#
+#     The above copyright notice and this permission notice shall be
+#     included in all copies or substantial portions of the Software.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#     THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#     DEALINGS IN THE SOFTWARE.
+
+import os, sys
+
+my_file = os.path.abspath(__file__)
+my_path ='/'.join(my_file.split('/')[0:-1])
+
+import time
+from PIL import Image
+from random import randrange, uniform
+import pygame
+from pygame.locals import *
+import random
+
+sys.path.insert(0,my_path + "/libs" )
+
+from MultiProcessing import MultiProcessing
+from SharedFloats import SharedFloats
+
+program_path = my_path + "/.."
+
+class FaceGfx(MultiProcessing):
+
+	_released 	= False
+	
+	_visualize	= True
+
+	screenWidth  = 800
+	screenHeight = 480
+	
+	_lastEyeX		= 0
+	_lastEyeY		= 0
+	
+	speaking = False
+	
+	mouthGfx = None
+	mouthAngle = 0
+	mouthAngleTarget = 0
+	mouthPosY = 0
+	mouthPosYTarget = 0
+	
+	glassesGfx = None
+	eyeBallGfx = None
+	eyeWhiteGfx = None
+	eyeBrowLeftGfx = None
+	eyeBrowRightGfx = None
+	i = 0
+	lcd = None
+	lastUpdate = time.time()
+	delaySeconds = 1.0 / 10 #10 fps
+
+	__shared_floats__			= SharedFloats(max_length=2)
+	__eye_x_float__ 			= __shared_floats__.get_next_key()
+	__eye_y_float__ 			= __shared_floats__.get_next_key()
+
+	def __init__(self, visualize ):
+		
+		super().__init__(prio=20)
+		
+		self.program_path = program_path
+		self._visualize = visualize
+		
+		image_filename = program_path + '/Gfx/Face-001.png'
+		
+		location= [0,0]
+				
+		os.putenv('SDL_FBDEV', '/dev/fb1')
+		pygame.init()
+		pygame.mixer.quit() # to prevent conflicts with speech output (audio device busy)
+
+		screenInfo = pygame.display.Info()
+		if (screenInfo.current_w > 900):
+			self.lcd = pygame.display.set_mode((800,480))
+		else:
+			self.lcd = pygame.display.set_mode((800,480), FULLSCREEN, 16)
+		
+		self.LoadImages()
+		
+		self.eyeX = 0.5
+		self.eyeY = 0.5
+
+		pygame.display.update()
+		pygame.mouse.set_visible(False)
+		self.lcd.fill((255,194,0))
+
+		pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+		self.image = pygame.image.load(image_filename)
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = location
+		self.bg = pygame.image.load(image_filename)
+		
+		self.PaintMouth(False)
+				
+		super().StartUpdating()
+		
+	## multi process properties START ##
+	
+	@property
+	def eyeX(self):
+		return self.__shared_floats__.get_value(self.__eye_x_float__)
+	@eyeX.setter
+	def eyeX(self, value):
+		self.__shared_floats__.set_value(self.__eye_x_float__,value)
+		
+	@property
+	def eyeY(self):
+		return self.__shared_floats__.get_value(self.__eye_y_float__)
+	@eyeY.setter
+	def eyeY(self, value):
+		self.__shared_floats__.set_value(self.__eye_y_float__, value)
+	
+	## multi process properties END ##
+
+	def LoadImage(self, filename, transparent):
+		face_path = self.program_path + '/Gfx/Face/'
+		png = pygame.image.load(face_path + filename)
+		png.convert()
+		if (transparent == True):
+			return png
+		surface = pygame.Surface(png.get_size())
+		surface = surface.convert()
+		surface.blit(png,(0,0))
+		return surface
+
+	def LoadImages(self):
+		self.mouthGfx = self.LoadImage('mouth.png', False)
+		self.glassesGfx = self.LoadImage( 'glasses.png', False)
+		self.eyeBallGfx = self.LoadImage( 'eye_ball.png', True)
+		self.eyeWhiteGfx  = self.LoadImage( 'eye_white.png',False)
+		self.eyeBrowLeftGfx  = self.LoadImage( 'eyebrow_left.png', False)
+		self.eyeBrowRightGfx  = self.LoadImage( 'eyebrow_right.png', False)
+		
+	def PaintEye(self,leftFactor):
+		eyeCenterX = self.screenWidth * (0.5 + leftFactor * 0.158)
+		eyeCenterY = self.screenHeight * 0.4
+		
+		eyeWhiteWidthHalf = self.screenWidth * 0.09
+		eyeWhiteHeightHalf =  self.screenHeight * 0.09
+		eyeBallWidthHalf = self.eyeBallGfx.get_width() / 2
+		eyeBallHeightHalf = self.eyeBallGfx.get_height() / 2
+		
+		eyeBallPosX = eyeWhiteWidthHalf * (self.eyeX - 0.5)
+		eyeBallPosY = eyeWhiteHeightHalf * (self.eyeY - 0.5)
+		
+		self.lcd.blit(self.eyeBallGfx, (eyeCenterX - eyeBallPosX - eyeBallWidthHalf, eyeCenterY + eyeBallPosY - eyeBallHeightHalf))
+		
+		eyeBrowY = self.screenHeight * 0.1
+		eyeBrowGfx = self.eyeBrowRightGfx
+		if (leftFactor > 0):
+			eyeBrowGfx = self.eyeBrowLeftGfx
+			
+		self.lcd.blit(eyeBrowGfx, (eyeCenterX  - eyeBrowGfx.get_width() / 2 , eyeBrowY - eyeBrowGfx.get_height() / 2))
+		
+	def PaintMouth(self, moving):
+		speedAngle = 2
+		if (self.mouthAngle > self.mouthAngleTarget+speedAngle):
+			self.mouthAngle = self.mouthAngle-speedAngle
+		else:
+			if (self.mouthAngle < self.mouthAngleTarget-speedAngle):
+				self.mouthAngle = self.mouthAngle+speedAngle
+			else:
+				if (self.speaking == True):
+					self.mouthAngleTarget = random.randint(-5, 5)
+				else:
+					self.mouthAngleTarget = 0
+				
+		# random mouth y pos
+		speedY = 3
+		if (self.mouthPosY > self.mouthPosYTarget + speedY):
+			self.mouthPosY = self.mouthPosY-speedY
+		else:
+			if (self.mouthPosY < self.mouthPosYTarget - speedY):
+				self.mouthPosY = self.mouthPosY+speedY
+			else:
+				if (self.speaking == True):
+					self.mouthPosYTarget = random.randint(-10, 10)
+				else:
+					self.mouthPosYTarget = 0
+
+		rotatedMouth = self.rot_center(self.mouthGfx, self.mouthAngle)
+		self.lcd.blit(rotatedMouth, (self.screenWidth / 2 - self.mouthGfx.get_width() / 2, self.screenHeight * 0.7 + self.mouthPosY))
+
+	def Update(self):
+		if (super().updating_ended == True):
+			return
+		
+		time.sleep(self.delaySeconds)
+		
+		toUpdate = False
+		
+		# eye-balls
+		if (self._lastEyeX != self.eyeX or self._lastEyeY != self.eyeY):
+			toUpdate = True
+			self.lcd.blit(self.glassesGfx, ((self.screenWidth- self.glassesGfx.get_width()) / 2, self.screenHeight * 0.4 - self.glassesGfx.get_height() / 2))
+			self.PaintEye(-1)
+			self.PaintEye(1)
+			self._lastEyeX = self.eyeX
+			self._lastEyeY = self.eyeY
+
+		#random mouth rotation
+		if (self.speaking == True):
+			toUpdate = True
+			self.PaintMouth(True)
+		
+		if (toUpdate == True):
+			pygame.display.update()
+		else:
+			time.sleep(0.5)
+		
+	def SetEyePos(self,eyePosX, eyePosY):
+		if (eyePosX != self.eyeX or eyePosY != self.eyeY):
+			self.changed = True
+			self.eyeX =eyePosX
+			self.eyeY =eyePosY
+		
+		
+	def SetScreenBackLight(self, onOff):
+		if (onOff == True):
+			os.system("echo 0 | sudo tee /sys/class/backlight/rpi_backlight/bl_power")
+		else:
+			os.system("echo 1 | sudo tee /sys/class/backlight/rpi_backlight/bl_power")
+		
+	#def UpdateEndless(self):
+	#	while self._ended == False:
+	#		self.Update()
+	#		time.sleep(0.02)
+		
+
+	def rot_center(self, image, angle):
+		"""rotate an image while keeping its center and size"""
+		orig_rect = image.get_rect()
+		rot_image = pygame.transform.rotate(image, angle)
+		rot_rect = orig_rect.copy()
+		rot_rect.center = rot_image.get_rect().center
+		rot_image = rot_image.subsurface(rot_rect).copy()
+		return rot_image
+		
+	def Release(self):
+		if (self._released == False):
+			print("facegfx releasing")
+			self._released = True
+			super().EndUpdating()
+			
+	def __del__(self):
+		self.Release()
+
+if __name__ == "__main__":
+	
+	face = FaceGfx(True) 
+	
+	face.speaking = True
+	#face.SetScreenBackLight(True)
+	
+	ended = False
+	
+	
+	
+	while ended == False:
+		
+		t = time.time()
+		
+		#face.Update()
+		#face.SetScreenBackLight(False)
+		
+		d = time.time() -t
+		#print(int(d * 1000))
+		
+		events = pygame.event.get()
+		for event in events:
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					ended = True 
+					
+		time.sleep(.01)
+		
+		newX = face.eyeX + 0.01
+		if (newX > 1):
+			newX =0
+		face.SetEyePos(newX, newX)
+		
+					
+		#face.SetScreenBackLight(True)
+					
+	face.Release()
+
---
+++ Python/Roobert.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+
+#      Roobert V2 - second version of home robot project
+#      ________            ______             _____ 
+#      ___  __ \______________  /_______________  /_
+#      __  /_/ /  __ \  __ \_  __ \  _ \_  ___/  __/
+#      _  _, _// /_/ / /_/ /  /_/ /  __/  /   / /_  
+#      /_/ |_| \____/\____//_.___/\___//_/    \__/
+#
+#     Project website: http://roobert.springwald.de
+#
+#     ##################################
+#     # Roobert master control program #
+#     ##################################
+#
+#     Licensed under MIT License (MIT)
+#
+#     Copyright (c) 2018 Daniel Springwald | daniel@springwald.de
+#
+#     Permission is hereby granted, free of charge, to any person obtaining
+#     a copy of this software and associated documentation files (the
+#     "Software"), to deal in the Software without restriction, including
+#     without limitation the rights to use, copy, modify, merge, publish,
+#     distribute, sublicense, and/or sell copies of the Software, and to permit
+#     persons to whom the Software is furnished to do so, subject to
+#     the following conditions:
+#
+#     The above copyright notice and this permission notice shall be
+#     included in all copies or substantial portions of the Software.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#     THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#     DEALINGS IN THE SOFTWARE.
+
+
+
+import os, sys
+from os.path import abspath
+
+import time
+from random import randrange, uniform
+import math
+import pygame
+
+from hardware.PCF8574 import PCF8574
+from hardware.I2cIoExpanderPcf8574 import I2cIoExpanderPcf8574
+from NeckLeftRight import NeckLeftRight
+from NeckUpDown import NeckUpDown
+#from SpeechOutput import SpeechOutput
+#from HandAndArm import HandAndArm
+from FaceGfx import FaceGfx
+from Camera import Camera
+#from Roomba import Roomba
+#from RgbLeds import *
+#from RelaisI2C import RelaisI2C
+from hardware.HardwareDevices import HardwareDevices
+from _thread import start_new_thread
+
+import atexit
+
+class Roobert:
+
+	I2cIoExpanderPcf8574Adress		= 0x38
+	MotorLeftRightAdress			= 0x0f
+	MotorUpDownAdress				= 0x3e
+	sens_3d_servo_adress			= 0x46
+	
+	_hardwareDevices 				= None
+	
+	FirstI2cIoExpanderPcf8574		= None
+	_roomba							= None
+	_faceGfx						= None
+	_neckLeftRight					= None
+	_neckUpDown						= None
+	_speechOutput					= None
+	_camera							= None
+	
+	_body_leds						= None
+	
+	_ended							= False
+	_released						= False
+
+	showFace						= True
+	_lastFace						= time.time() - 1000
+	
+	_rotate							= 1
+
+	def __init__(self):
+
+		self._hardwareDevices = HardwareDevices.singleton()
+
+		self.FirstI2cIoExpanderPcf8574 = I2cIoExpanderPcf8574(self.I2cIoExpanderPcf8574Adress, useAsInputs=True)
+		endStop = self.FirstI2cIoExpanderPcf8574
+		
+		self._neckUpDown = NeckUpDown(I2cIoExpanderPcf8574(self.MotorUpDownAdress, useAsInputs=False), endStop)
+		self._neckUpDown.targetPos =int(self._neckUpDown.MaxSteps * 0.85)
+		
+		self._neckLeftRight = NeckLeftRight(self.MotorLeftRightAdress, endStop)
+		self._neckLeftRight.targetPos = int(self._neckLeftRight.MaxSteps *0.25)
+
+		self._speechOutput = SpeechOutput()
+				
+		self._faceGfx = FaceGfx(self.showFace)
+				
+		self._body_leds	= self._hardwareDevices.body_leds
+		
+		self._camera = Camera() 		
+
+		
+	def Release(self):
+		if (self._released == False):
+			self._released = True
+			print ("shutting down roobert")
+			
+			# shut down other treads
+			
+			if (self._camera != None):
+				self._camera.Release()
+				
+			if (self._faceGfx != None):
+				self._faceGfx.Release()
+				
+			if (self._neckUpDown != None):
+				self._neckUpDown.Release()
+			if (self._neckLeftRight != None):
+				self._neckLeftRight.Release()
+				
+			if (self._hardwareDevices != None):
+				self._hardwareDevices.Release()
+				
+			self._ended = True
+			
+	def Update(self):
+		if (self.showFace== True):
+			self._faceGfx.speaking = self._speechOutput.speaking
+			
+	def RandomHeadMovement(self):
+		if (self._neckLeftRight.targetReached==True):
+			movementArea = int(self._neckLeftRight.MaxSteps / 2)
+			self._neckLeftRight.targetPos =randrange(0,movementArea)
+		if (self._neckUpDown.targetReached==True):
+			movementArea = int(self._neckUpDown.MaxSteps / 2)
+			self._neckUpDown.targetPos =randrange(0, movementArea)
+			
+	def Greet(self):
+		print("greetings!");
+		self._speechOutput.Speak("Guten Tag.")
+		self._speechOutput.Speak("Mein Name ist Robert")
+		self._hardwareDevices.hand_arm_right.gestureGreet()
+		while (self._speechOutput.speaking==True):
+			time.sleep(1)
+		self._speechOutput.Speak("Ich freue mich, Sie kennen zu lernen")
+		self._hardwareDevices.hand_arm_right.gesturePointForward()
+		while (self._speechOutput.speaking==True):
+			time.sleep(1)
+		self._hardwareDevices.hand_arm_right.home()
+			
+	def drive_avoiding_obstacles(self):
+		#print (self._sens3d.nearest)
+		if (self._sens3d.nearest > 40):
+			self._roomba.move(3)
+			#time.sleep(0.5)
+		else:
+			if (self._sens3d.x_weight < 0.45):
+				self._roomba.rotate(-20)
+			else:
+				if (self._sens3d.x_weight > 0.55):
+					self._roomba.rotate(20)
+				else:
+					self._roomba.rotate(180 * self._rotate)
+					self._rotate = self._rotate * -1
+					
+	def RotateDemoForPhoto(self):
+		self._hardwareDevices.hand_arm_right.gesturePointForward();
+		#self._hardwareDevices.roomba.rotate(10);
+		
+					
+	def FollowFace(self):
+		faceX = self._camera.posXFace
+		faceY = self._camera.posYFace
+		timeSinceLastFace = time.time() - self._lastFace 
+		if (faceX != -1 and faceY != -1):
+			# oh, there is a face
+			if (timeSinceLastFace > 60):
+				# see a face after a long time: say hello!
+				start_new_thread(self.Greet,())
+				#self.Greet()
+				a=0
+				
+			self._lastFace = time.time()
+			self._camera.ResetFace()
+			diffX = (faceX - 0.5) 
+			diffY = (faceY - 0.5) 
+			if (math.fabs(diffX ) > 0.1):
+				self._neckLeftRight.targetPos = self._neckLeftRight.targetPos - int(diffX * 100)
+			if (math.fabs(diffY ) > 0.1):
+				self._neckUpDown.targetPos = self._neckUpDown.targetPos - int(diffY * 100)
+			
+			self._faceGfx.SetEyePos(faceX,faceY)
+		else:
+			# there is no face
+			if (timeSinceLastFace > 20):
+				# long time no face seen
+				self._faceGfx.SetEyePos(0.5,0.5)
+			
+	def __del__(self):
+		self.Release()
+		
+def exit_handler():
+	roobert.Release()
+
+if __name__ == "__main__":
+	
+	roobert = Roobert()
+	
+	atexit.register(exit_handler)
+	
+	ended = False
+	
+	roobert.Update()
+	#roobert._handArm.setArm(100,100,100)
+	
+	#roobert._speechOutput.Speak("Hallo")
+	
+	#roobert.Greet();
+	
+	while ended == False:
+		
+		time.sleep(0.5)
+		
+		roobert.Update()
+		
+		#roobert.RotateDemoForPhoto();
+		
+		#roobert.RandomHeadMovement()
+		
+		roobert.FollowFace()
+		#roobert.drive_avoiding_obstacles()
+
+		events = pygame.event.get()
+	
+		for event in events:
+			if event.type == pygame.MOUSEBUTTONUP:
+				ended = True 
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					ended = True 
+				if event.key == pygame.K_TAB:
+					#roobert.Greet()
+					#start_new_thread(roobert.Greet,())
+					a=0
+
+        
+        
+        
+        
+
+    
+
+
+
+
+
